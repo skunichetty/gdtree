ac3bacd7bd2e3364edd241881533d49f45394d25
skunichetty <skunich@umich.edu>
2021-07-31 19:37:06 -0400

    Refactored + Retested traversal history

---
+++ pytree/end_state_history.py
@@ -0,0 +1,157 @@
+from typing import List
+import pytree.constants as constants
+
+
+class EndStateHistory:
+    """
+    A container that holds the end state history as encountered during traversal. An end state is a boolean values
+    storeing if this entry exists as the last element in an lexicographical ordering
+    of the directory tree. EndStateHistory is a container of these values for each level of the directory tree
+    traversed for a given entry.
+    """
+
+    def __init__(self, input_history: List[bool] = None):
+        """
+        Initializes the EndStateHistory container.
+
+        Args:
+            input_history (List[bool], optional): An boolean list holding the sequential end state history.
+            The order of the array is read from least depth in tree to greatest depth in tree. Defaults to None.
+
+        Raises:
+            ValueError: Raises if the input end state history list length exceeds the maximum allowed depth
+        """
+        self.history = 0
+        self.depth = 0
+        if input_history is not None and len(input_history) > 0:
+            if len(input_history) > constants.MAX_DEPTH:
+                raise ValueError(
+                    "Input list has too many values (greater than maximum allowed depth)"
+                )
+            self.depth = len(input_history)
+            for index, state in enumerate(input_history):
+                self.history |= state << index
+
+    def __len__(self) -> int:
+        """
+        Gets the length of the history stored, corresponding to the depth of the traversal history up to that point.
+
+        Returns:
+            int: The length of this history
+        """
+        return self.depth
+
+    def __getitem__(self, key: int) -> bool:
+        """
+        Gets the end state value at the given depth
+
+        Args:
+            key (int): The depth to retrieve the end state value at
+
+        Raises:
+            IndexError: Raises if the depth given exceeds the bounds of the history
+
+        Returns:
+            bool: The end state value at the depth given
+        """
+        if key >= self.depth or key < 0:
+            raise IndexError("Index %d outside of history bounds" % key)
+        mask = 1 << key
+        return bool(self.history & mask)
+
+    def __setitem__(self, key: int, value: bool) -> None:
+        """
+        Sets the end state value at the given index
+
+        Args:
+            key (int): The index to retrieve the end state value at
+            value (bool): The end state value to set at the given index
+
+        Raises:
+            IndexError: Raises if the index given exceeds the length of the current instance
+        """
+        if key >= self.depth or key < 0:
+            raise IndexError("Index %d outside of history bounds" % key)
+        mask = 1 << key
+        if value:
+            self.history |= mask
+        else:
+            self.history &= ~mask
+
+    def append(self, value: bool) -> None:
+        """
+        Appends new end state value to the current instance of EndStateHistory
+
+        Args:
+            value (bool): The new end state value to append
+
+        Raises:
+            DepthError: Raises if attempt is made to extend end state beyond maximum depth
+        """
+        if self.depth == constants.MAX_DEPTH:
+            raise DepthError(
+                "Attempted to extend state history beyond maximum allowed traversal depth"
+            )
+        prev_depth = self.depth
+        self.depth += 1
+        self[prev_depth] = value
+
+    def __iter__(self):
+        """
+        Returns an iterator to traverse the history
+
+        Returns:
+            _StateHistoryIterator: Iterator to iterate through this instance of EndStateHistory
+        """
+        return _StateHistoryIterator(self)
+
+    def extend(self, other) -> None:
+        """
+        Extends this instance of EndStateHistory with the history from another. Appends the
+        history values from the other history to the end of this instances container. The
+        other instance is not mutated.
+
+        Args:
+            other (EndStateHistory): The other EndStateHistory whose values will be extended
+
+        Raises:
+            DepthError: Raises if extension by other history will exceed maximum allowed traversal depth
+        """
+        if (self.depth + other.depth) > constants.MAX_DEPTH:
+            raise DepthError(
+                "Attempted to extend state history beyond maximum allowed traversal depth"
+            )
+        other_history = other.history
+        other_history <<= self.depth
+        self.history |= other_history
+
+
+class _StateHistoryIterator:
+    """
+    An iterator meant to iterate through EndStateHistory
+    """
+
+    def __init__(self, history: EndStateHistory):
+        self.index = 0
+        self.history = history
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.history):
+            result = self.history[self.index]
+            self.index += 1
+            return result
+        else:
+            raise StopIteration
+
+
+class DepthError(Exception):
+    """
+    An error called when a DirectoryTreeEntry or traversal function attempts
+    to exceed the maximum program depth
+    """
+
+    def __init__(self, message: str):
+        self.message = message
---
+++ test/test_end_state_history.py
@@ -0,0 +1,185 @@
+from unittest import TestCase, main
+import pytree.constants as constants
+from pytree.end_state_history import DepthError, EndStateHistory
+
+
+class TestEndStateHistory(TestCase):
+    def test_construction_default_length(self):
+        """
+        Tests that default construction produces the correct depth
+        """
+        entry = EndStateHistory()
+        self.assertEqual(len(entry), 0)
+
+    def test_construction_length_error(self):
+        """
+        Tests that the length of the input boolean array does not exceed
+        maximum allowed depth
+        """
+        history = [False] * (constants.MAX_DEPTH + 1)
+        with self.assertRaises(ValueError):
+            entry = EndStateHistory(history)
+
+    def test_construction_empty_input_length(self):
+        """
+        Tests that an empty input array will correctly process length
+        """
+        entry = EndStateHistory([])
+        self.assertEqual(len(entry), 0)
+
+    def test_len_empty(self):
+        """
+        Tests that the history length is 0 when no values are added
+        """
+        entry = EndStateHistory()
+        self.assertEqual(len(entry), 0)
+
+    def test_len_nonempty(self):
+        """
+        Tests that the history length is properly returned when no values are added
+        """
+        history = [True, False, True, True, False, False, False]
+        entry = EndStateHistory(history)
+        self.assertEqual(len(entry), 7)
+
+    def test_len_max_length(self):
+        """
+        Tests that the history length is properly returned at maximum depth
+        """
+        history = [False] * constants.MAX_DEPTH
+        entry = EndStateHistory(history)
+        self.assertEqual(len(entry), constants.MAX_DEPTH)
+
+    def test_history_append_length(self):
+        """
+        Tests that adding a new value to history increases history length
+        """
+        entry = EndStateHistory([True, False, True, False, False, True])
+        entry.append(True)
+        self.assertEqual(len(entry), 7)
+
+    def test_history_append_length(self):
+        """
+        Tests that adding a new value to history adds a new value to the end
+        """
+        entry = EndStateHistory([True, False, True, False, False, True])
+        entry.append(False)
+        self.assertFalse(entry[6])
+
+    def test_history_append_error(self):
+        """
+        Tests that an error is raised when trying to update history
+        past the max possible depth
+        """
+        entry = EndStateHistory([False] * constants.MAX_DEPTH)
+        with self.assertRaises(DepthError):
+            entry.append(True)
+
+    def test_history_append_start(self):
+        """
+        Tests that a new value is successfully added at the start
+        """
+        entry = EndStateHistory()
+        entry.append(True)
+        self.assertTrue(entry[0])
+
+    def test_iteration(self):
+        """
+        Tests that history is iterated through in the correct order
+        """
+        history = [True, False, True, True, False]
+        entry = EndStateHistory(history)
+        for value, expected_value in zip(entry, history):
+            self.assertEqual(value, expected_value)
+        for value in entry:
+            print(value)
+
+    def test_access(self):
+        """
+        Tests that history can be easily accessed
+        """
+        entry = EndStateHistory([True, False, True, False, False, True])
+        self.assertFalse(entry[3])
+        self.assertTrue(entry[2])
+
+    def test_access_error(self):
+        """
+        Tests that an error is thrown when accessing an invalid index
+        """
+        entry = EndStateHistory([True, False, True, False, False, True])
+        with self.assertRaises(IndexError):
+            self.assertTrue(entry[6])
+
+    def test_mutate(self):
+        """
+        Tests that history can be easily mutated
+        """
+        entry = EndStateHistory([True, False, True, False, False, True])
+        self.assertTrue(entry[0])
+        entry[0] = False
+        self.assertFalse(entry[0])
+
+    def test_mutate_error(self):
+        """
+        Tests that an error is thrown when accessing an invalid index
+        """
+        entry = EndStateHistory([True, False, True, False, False, True])
+        with self.assertRaises(IndexError):
+            entry[6] = True
+
+    def test_extend(self):
+        """
+        Tests that the history can be extended with another history
+        """
+        entry_one = EndStateHistory([True, False, True])
+        entry_two = EndStateHistory([False, False, True])
+        output_values = [True, False, True, False, False, True]
+        entry_one.extend(entry_two)
+        for state, expected_output in zip(entry_one, output_values):
+            self.assertEqual(state, expected_output)
+
+    def test_extend_error(self):
+        """
+        Tests that a depth error is raised when attempting to extend
+        one history past the maximum depth
+        """
+        entry_one = EndStateHistory([False] * constants.MAX_DEPTH)
+        entry_two = EndStateHistory([False, False, True])
+        with self.assertRaises(DepthError):
+            entry_one.extend(entry_two)
+
+    def test_extend_error_edge(self):
+        """
+        Tests that a depth error is raised when attempting to extend
+        one history exactly one past the maximum depth
+        """
+        entry_one = EndStateHistory([False] * (constants.MAX_DEPTH - 2))
+        entry_two = EndStateHistory([False, False, True])
+        with self.assertRaises(DepthError):
+            entry_one.extend(entry_two)
+
+    def test_extend_empty(self):
+        """
+        Tests that a history can be extended from empty values without any issue
+        """
+        entry_one = EndStateHistory()
+        entry_two = EndStateHistory([False, False, True])
+        output_values = [False, False, True]
+        entry_one.extend(entry_two)
+        for state, expected_output in zip(entry_one, output_values):
+            self.assertEqual(state, expected_output)
+
+    def test_extend_no_change(self):
+        """
+        Tests that extending a history from one instance does not change the values of the other instance.
+        """
+        entry_one = EndStateHistory([True, False, True])
+        entry_two = EndStateHistory([False, False, True])
+        output_values = [False, False, True]
+        entry_one.extend(entry_two)
+        for state, expected_output in zip(entry_two, output_values):
+            self.assertEqual(state, expected_output)
+
+
+if __name__ == "__main__":
+    main()
